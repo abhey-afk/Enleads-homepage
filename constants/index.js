2b0a388e2c7bbc3d35218806f32fbcf052c2fb1e
Abhey Mishra <97222685+abhey-afk@users.noreply.github.com>
2024-10-24 22:57:51 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,8 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/about', key: 'about', label: 'About' },
+  { href: '/career', key: 'career', label: 'Career' },
+  { href: '/faculty', key: 'faculty', label: 'Faculty' },
+  { href: '/student-life', key: 'student_life', label: 'Student Life' },
+];
